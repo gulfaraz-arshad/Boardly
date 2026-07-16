0965ed21414270a8cbde439c843d467f744063ed
Gulfaraz <gul.cruisebrains@gmail.com>
2026-07-16 23:16:29 +0500

    fix: fix Flux styling & dark mode inconsistencies in production.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,11 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'Laravel' }}</title>
+<title>{{ $title ?? 'Boardly' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
